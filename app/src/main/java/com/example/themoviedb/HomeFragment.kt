e2dec7f902cffe221abb2a194cf2c0348cae65bc
facorrea700 <facorrea179@gmail.com>
2021-11-02 14:26:24 -0300

    nothing changes

--- app/src/main/java/com/example/themoviedb/HomeFragment.kt
+++ app/src/main/java/com/example/themoviedb/HomeFragment.kt
@@ -5,18 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.setupWithNavController
 
 import com.example.themoviedb.databinding.FragmentHomeBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
-    val navView: BottomNavigationView = binding.bottomNavigationView
-    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,15 +18,10 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
 
-        navController = findNavController()
-        navView.setupWithNavController(navController)
-
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
     }
-
 }
 
